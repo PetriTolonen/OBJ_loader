40d003f8a0d70873a75805ba0167c412edce8a8a
PetriTolonen <TTV14SPetriT@kamk.fi>
2015-12-01 14:31:33 +0200

    Textures

--- OpenGL_template/OpenGL_template/Main.cpp
+++ OpenGL_template/OpenGL_template/Main.cpp
@@ -54,42 +54,12 @@ void InitBox(){
 
 	programID = LoadShaders("VertexShader.vertexshader", "FragmentShader.fragmentshader");
 	MVP_MatrixID = glGetUniformLocation(programID, "MVP");
-	wh_VectorID = glGetUniformLocation(programID, "wh");
 	
 	std::vector<glm::vec3> vertices;
 	std::vector<glm::vec2> uvs;
 	std::vector<glm::vec3> normals;
 	
-	res.loadOBJ("cube.obj", vertices, uvs, normals);
-
-	//static const GLfloat g_vertex_buffer_data[] = {
-	//	0.0f, 0.0f, 0.0f,
-	//	1.0f, 0.0f, 0.0f,
-	//	0.0f, 1.0f, 0.0f,
-	//	1.0f, 1.0f, 0.0f,
-	//};
-
-	//glGenBuffers(1, &vertexbuffer);
-	//glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
-	//static const GLfloat g_color_buffer_data[] = {
-	//	1.0f, 0.0f, 0.0f, 1.0f,
-	//	0.0f, 1.0f, 0.0f, 1.0f,
-	//	0.0f, 0.0f, 1.0f, 1.0f,
-	//	0.1f, 1.0f, 1.0f, 1.0f,
-	//};
-
-	//glGenBuffers(1, &colorbuffer);
-	//glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
-
-	//static const GLubyte g_indices[] = {
-	//	0, 1, 2, 1, 3, 2,
-	//};
-	//glGenBuffers(1, &indexbuffer);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
-	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_indices), g_indices, GL_STATIC_DRAW);
+	res.loadOBJ("cube2.obj", vertices, uvs, normals);
 
 
 	glGenBuffers(1, &vertexbuffer);
@@ -101,17 +71,25 @@ void InitBox(){
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glBufferData(GL_ARRAY_BUFFER, uvs.size() * sizeof(glm::vec2), &uvs[0], GL_STATIC_DRAW);
 
+	TextureID = glGetUniformLocation(programID, "myTextureSampler");
+	
+	Texture = loadBMP_custom("./uvtemplate.bmp");
 }
 void DrawBox(){
 	glEnable(GL_BLEND);
 
 	glUseProgram(programID);
 
-	M = glm::translate(glm::vec3(cos(alpha),sin(alpha),cos(alpha)))*glm::rotate(alpha, glm::vec3(1.0f,1.0f,1.0f));
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, Texture);
+	// Set our "myTextureSampler" sampler to user Texture Unit 0
+	glUniform1i(TextureID, 0);
 
+	//M = glm::translate(glm::vec3(cos(alpha),sin(alpha),cos(alpha)))*glm::rotate(alpha, glm::vec3(1.0f,1.0f,1.0f));
+	M = glm::translate(glm::vec3(cos(alpha), 0.0f, 0.0f));
 	alpha += 0.1;
 
-	MVP = VP*M;
+	MVP = VP/**M*/;
 	glUniformMatrix4fv(MVP_MatrixID, 1, GL_FALSE, &MVP[0][0]);
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -127,7 +105,7 @@ void DrawBox(){
 	glEnableVertexAttribArray(1);
 	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
 	glVertexAttribPointer(			
-		VERTEX_COLOR,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+		TEXTURE_DATA,                               // attribute. No particular reason for 1, but must match the layout in the shader.
 		2,                               
 		GL_FLOAT,                         // type
 		GL_FALSE,                         // normalized?
@@ -142,95 +120,11 @@ void DrawBox(){
 	glDisableVertexAttribArray(1);
 }
 
-//void InitOctagon(){
-//	glGenVertexArrays(1, &textureVertexArrayID);
-//	glBindVertexArray(textureVertexArrayID);
-//
-//	textureProgramID = LoadShaders("Texture.VertexShader", "Texture.FragmentShader");
-//	
-//	MVP_MatrixID = glGetUniformLocation(textureProgramID, "MVP");
-//
-//	static const GLfloat g_vertex_buffer_data[] = {
-//		0.0f, 0.0f, 0.0f,
-//		512*1.0f, 0.0f, 0.0f,
-//		0.0f, 512*1.0f, 0.0f,
-//		512*1.0f, 512*1.0f, 0.0f,
-//	};
-//
-//	glGenBuffers(1, &texturevertexbuffer);
-//	glBindBuffer(GL_ARRAY_BUFFER, texturevertexbuffer);
-//	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-//
-//	static const GLubyte g_indices[] = {
-//		0, 1, 2, 1, 3, 2,
-//	};
-//	glGenBuffers(1, &textureindexbuffer);
-//	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexbuffer);
-//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(g_indices), g_indices, GL_STATIC_DRAW);
-//
-//	static const GLfloat g_uv_buffer_data[] = {
-//		0.0f, 0.0f,
-//		1.0f, 0.0f,
-//		0.0f, 1.0f,
-//		1.0f, 1.0f,
-//	};
-//
-//	glGenBuffers(1, &uvbuffer);
-//	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-//	glBufferData(GL_ARRAY_BUFFER, sizeof(g_uv_buffer_data), g_uv_buffer_data, GL_STATIC_DRAW);
-//	TextureID = glGetUniformLocation(programID, "myTextureSampler");
-//
-//	numberofind = sizeof(g_indices) / sizeof(g_indices[0]);
-//
-//	Texture = loadBMP_custom("./uvtemplate2.bmp");	
-//}
-//
-//void DrawOctagon(){
-//	glEnable(GL_BLEND);
-//
-//	glUseProgram(textureProgramID);
-//
-//	glActiveTexture(GL_TEXTURE0);
-//	glBindTexture(GL_TEXTURE_2D, Texture);
-//	// Set our "myTextureSampler" sampler to user Texture Unit 0
-//	glUniform1i(TextureID, 0);
-//
-//	MVP = VP*M;
-//	glUniformMatrix4fv(MVP_MatrixID, 1, GL_FALSE, &MVP[0][0]);
-//	glEnableVertexAttribArray(0);
-//	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-//	glVertexAttribPointer(
-//		VERTEX_POSITION, //layout in the shader.
-//		3,       // size
-//		GL_FLOAT,// type
-//		GL_FALSE,// normalized
-//		0,       // stride
-//		(void*)0 // array buffer offset
-//		);
-//
-//	glEnableVertexAttribArray(1);
-//	glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
-//	glVertexAttribPointer(
-//		TEXTURE_DATA,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-//		2,                                // size : U+V => 2
-//		GL_FLOAT,                         // type
-//		GL_FALSE,                         // normalized?
-//		0,                                // stride
-//		(void*)0                          // array buffer offset
-//		);
-//	glDrawElements(GL_TRIANGLES, numberofind, GL_UNSIGNED_BYTE, (GLvoid*)0);
-//	
-//
-//	//std::cout << glm::degrees(alpha2) << " - " << roundround2 << std::endl;
-//	glDisableVertexAttribArray(0);
-//	glDisableVertexAttribArray(1);
-//}
-
 void Init(void) {
 	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 	glEnable(GL_DEPTH_TEST);
 
-	glm::vec3 cameraPos = glm::vec3(3.0f, 3.0f, 10.0f);
+	glm::vec3 cameraPos = glm::vec3(3.0f, 3.1f, 10.0f);
 	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
